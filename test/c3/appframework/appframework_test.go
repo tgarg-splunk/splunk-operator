77f98fc8564c02fac62fb0e9bcf068b76bef0f6d
Vivek Reddy <vivekr@splunk.com>
2022-02-16 20:57:09 -0800

    testing 1 test in develop branch

--- test/c3/appframework/appframework_test.go
+++ test/c3/appframework/appframework_test.go
@@ -59,7 +59,7 @@ var _ = Describe("c3appfw test", func() {
 		}
 	})
 
-	XContext("Single Site Indexer Cluster with Search Head Cluster (C3) and App Framework", func() {
+	Context("Single Site Indexer Cluster with Search Head Cluster (C3) and App Framework", func() {
 		It("smoke, c3, appframework: can deploy a C3 SVA with App Framework enabled, install apps then upgrade them", func() {
 
 			/* Test Steps
